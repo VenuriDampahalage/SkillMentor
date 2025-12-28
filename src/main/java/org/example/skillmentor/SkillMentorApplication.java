82f64f3c7b530c76d546b13db57c1804f6ebb1d1
VenuriDampahalage <dampahalagevenuri@gmail.com>
2025-12-28 17:20:13 +0530

    Initial commit

---
+++ src/main/java/org/example/skillmentor/SkillMentorApplication.java
@@ -0,0 +1,13 @@
+package org.example.skillmentor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillMentorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkillMentorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/skillmentor/SkillMentorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.skillmentor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillMentorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
