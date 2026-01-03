23f120af6e8fb12d5d6e858d4e031799d7a2c7c1
VenuriDampahalage <dampahalagevenuri@gmail.com>
2026-01-03 12:58:38 +0530

    Create modelMapperConfig.java

---
+++ src/main/java/online/Skill_Mentor/configs/modelMapperConfig.java
@@ -0,0 +1,19 @@
+package online.Skill_Mentor.configs;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class modelMapperConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+
+        modelMapper.getConfiguration().setSkipNullEnabled(true);
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.LOOSE);
+
+        return modelMapper;
+    }
+}
