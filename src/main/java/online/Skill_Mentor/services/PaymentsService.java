b933da26ff2bf260530399b245207019c3275db8
VenuriDampahalage <dampahalagevenuri@gmail.com>
2026-01-04 20:42:01 +0530

    Create PaymentsService.java

---
+++ src/main/java/online/Skill_Mentor/services/PaymentsService.java
@@ -0,0 +1,15 @@
+package online.Skill_Mentor.services;
+
+import online.Skill_Mentor.entities.Payments;
+
+import java.util.List;
+
+public interface PaymentsService {
+    List<Payments> getPayments();
+    Payments getPaymentById(Long id);
+    Payments createPayment(Payments payment);
+    Payments updatePayment(Long id, Payments  payment);
+    Payments patchPayment(Long id, Payments  payment);
+    void deletePaymentById(Long id);
+    List<Payments> getOrderPayments(List<Payments> payments);
+}
