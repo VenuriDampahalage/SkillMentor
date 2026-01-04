ff00ce4abfffec76022ea18e681994ef81c3f918
VenuriDampahalage <dampahalagevenuri@gmail.com>
2026-01-04 14:32:03 +0530

    Delete SubjectRepository.java

--- src/main/java/online/Skill_Mentor/repositories/SubjectRepository.java
+++
@@ -1,7 +0,0 @@
-package online.Skill_Mentor.repositories;
-
-import online.Skill_Mentor.entities.Subject;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubjectRepository extends JpaRepository<Subject, Long> {
-}
