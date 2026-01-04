7351db7d48b219f287c5603a06e75f35374be7da
VenuriDampahalage <dampahalagevenuri@gmail.com>
2026-01-04 20:41:57 +0530

    Create PaymentServiceImpl.java

---
+++ src/main/java/online/Skill_Mentor/services/impl/PaymentServiceImpl.java
@@ -0,0 +1,15 @@
+package online.Skill_Mentor.services.impl;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class PaymentServiceImpl {
+
+
+}
