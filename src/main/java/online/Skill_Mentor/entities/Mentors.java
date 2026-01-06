8288537bd4131d9a48ede87acfc0335f8519e9fa
VenuriDampahalage <dampahalagevenuri@gmail.com>
2026-01-06 23:21:54 +0530

    Update Mentors.java

--- src/main/java/online/Skill_Mentor/entities/Mentors.java
+++ src/main/java/online/Skill_Mentor/entities/Mentors.java
@@ -8,6 +8,7 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.Date;
 
 @Entity
@@ -19,41 +20,45 @@ public class Mentors {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long Id;
+    private Long Id; //1
 
     @Column(length = 100, nullable = false, unique = true)
-    private String email;
+    private String email; //2
 
     @Column(length = 100, nullable = false)
-    private String first_Name;
+    private String first_Name; //3
 
     @Column(length = 100, nullable = false)
-    private String last_Name;
+    private String last_Name; //4
 
     @Column(length = 100, nullable = false)
-    private String profession;
+    private String profession; //5
 
     @Column(length = 100, nullable = false)
-    private String company;
+    private String company; //6
 
     @Column(nullable = false, updatable = true)
-    private int experience_years;
+    private int experience_years; //7
 
     @Column(length = 1000, nullable = false)
-    private String bio;
+    private String bio; //8
 
     @Column(name = "availability_Start", updatable = true)
-    private LocalDateTime availability_Start;
+    private LocalDateTime availability_Start; //9
 
     @Column(name = "availability_End", updatable = true)
-    private LocalDateTime availability_End;
+    private LocalDateTime availability_End; //10
 
     @CreationTimestamp
     @Column(name = "created_at", nullable = false, updatable = false)
-    private Date createdAt;
+    private Date createdAt; //11
 
     @UpdateTimestamp
     @Column(name = "updated_at")
-    private Date updatedAt;
+    private Date updatedAt; //12
+
+    //One Mentor belongs to many sessions
+    @OneToMany(mappedBy = "mentor_id")
+    private Collection<Sessions> sessions;
 
 }
